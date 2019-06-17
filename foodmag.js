dc3c0f5ce18defabac67b773b3939aa7c1a40f21
Joe Vaughn <31394713+olethrosjv@users.noreply.github.com>
2019-06-17 09:58:21 -0700

    Add files via upload

---
+++ foodmag.js
@@ -0,0 +1,24 @@
+/* Joe Vaughn
+03/18/19
+foodmag.js
+*/
+
+$( document ).ready(function() {
+
+$( ".cross" ).hide();
+$( ".menu" ).hide();
+$( ".hamburger" ).click(function() {
+$( ".menu" ).slideToggle( "slow", function() {
+$( ".hamburger" ).hide();
+$( ".cross" ).show();
+});
+});
+
+$( ".cross" ).click(function() {
+$( ".menu" ).slideToggle( "slow", function() {
+$( ".cross" ).hide();
+$( ".hamburger" ).show();
+});
+});
+
+});
